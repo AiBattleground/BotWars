5402fce64912e797c772e90475fc0c2cbe484c99
Zachary <zach@zachshuford.com>
2014-10-10 20:00:40 -0400

    Really fixed the interface this time.

--- NetBots.Bot.Interface/BotletMoveCollection.cs
+++ NetBots.Bot.Interface/BotletMoveCollection.cs
@@ -2,12 +2,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 
 namespace NetBots.Bot.Interface
 {
-    public class BotletMoveCollection : List<BotletMove>
+    public class BotletMoveCollection : IBotLetMoveCollection
     {
+        private readonly List<BotletMove> _moves = new List<BotletMove>(); 
+
+        public IEnumerable<BotletMove> Moves { get { return _moves; } }
         public string Color { get; set; }
     }
 }
