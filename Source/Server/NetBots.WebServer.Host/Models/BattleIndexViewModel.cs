b8e1e0e6a143a221847e8177a53723bf1802fa1f
Patrick Graham <pabreetzio@gmail.com>
2014-11-04 00:23:42 -0500

    removing stowaway projects

---
+++ Source/Server/NetBots.WebServer.Host/Models/BattleIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetBots.WebServer.Host.Models
+{
+    public class BattleIndexViewModel
+    {
+        public int Bot1Id { get; set; }
+        public int Bot2Id { get; set; }
+    }
+}
