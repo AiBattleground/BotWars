c4d9db29d0879c1a67aeb2d34d7f77b9ea2df8d9
Pharylon <zach@zachshuford.com>
2014-11-11 13:38:56 -0500

    Added failing unit test for diagonal moves

--- Source/NetBots.Tests/EngineTests.cs
+++ Source/NetBots.Tests/EngineTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NetBots.Core;
@@ -41,6 +42,34 @@ namespace NetBots.Tests
             var grid = gameState.Grid;
             Assert.IsTrue(grid.Count(x => x == '1') == grid.Count(x => x == '2'));
             Assert.IsTrue(gameState.P1.Energy == gameState.P2.Energy);
+        }
+
+
+        [TestMethod]
+        public void Issue18_DiagonalMoveBug()
+        {
+            var gameState = new GameState();
+            gameState.Cols = 6;
+            gameState.Rows = 6;
+            gameState.MaxTurns = 5;
+            gameState.TurnsElapsed = 0;
+            gameState.Grid = "1....." +
+                             "......" +
+                             "......" +
+                             "......" +
+                             "......" +
+                             "......";
+            gameState.P1 = new Player() { Spawn = 0 };
+            gameState.P2 = new Player() { Spawn = gameState.Grid.Length - 1 };
+            var game = new Game(gameState, "", "");
+            var p1moves = new PlayerMoves()
+            {
+                PlayerName = "p1",
+                Moves = new[] { new BotletMove(0, 7) }
+            };
+            game.UpdateGameState(new[] { p1moves });
+            var grid = gameState.Grid;
+            Assert.IsTrue(grid.IndexOf('1') != 7);
         }
     }
 }
