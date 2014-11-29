c0ab8de39b576d0c277f8789ca32cafded32eeae
Zachary <zach@zachshuford.com>
2014-11-29 13:08:04 -0500

    Removed some references to removed projects taht we didn't need, but was preventing build

--- Source/Engine/GameEngine/Game.cs
+++ Source/Engine/GameEngine/Game.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using NetBots.Bot.Interface;
 using NetBots.Core;
 
 namespace NetBots.GameEngine
--- Source/Engine/GameEngine/PlayerMoves.cs
+++ Source/Engine/GameEngine/PlayerMoves.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using NetBots.Bot.Interface;
 
 namespace NetBots.GameEngine
 {
