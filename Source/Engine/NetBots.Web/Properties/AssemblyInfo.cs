1c699e1cbfaf17dbfd1d77d0f24b7f3f0f74e1c9
Zachary <zach@zachshuford.com>
2014-11-29 13:02:13 -0500

    Removed GrahamBot, moving to another repo

--- Source/Engine/NetBots.Web/Properties/AssemblyInfo.cs
+++ Source/Engine/NetBots.Web/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NetBots.Web")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("NetBots Web Models")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Pharylon")]
 [assembly: AssemblyProduct("NetBots.Web")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © Pharylon, Patrick Graham 2014")]
+[assembly: AssemblyTrademark("Copyright © Pharylon, Patrick Graham 2014")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
